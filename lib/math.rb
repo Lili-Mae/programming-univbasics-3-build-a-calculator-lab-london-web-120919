a339aeba4dea2761f55dd1a690352622ccf49203
Lili-Mae Edwards <lilimae@hotmail.co.uk>
2019-12-04 11:37:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,19 +4,22 @@ puts sum
 end
 
 def subtraction(num1, num2)
-
+sum = num1 - num2
+puts sum 
 end
 
 def division(num1, num2)
-
+sum = num1 / num2
+puts sum
 end
 
 def multiplication(num1, num2)
-
+sum = num1 * num2
+puts sum
 end
 
 def modulo(num1, num2)
-
+sum = num1 / num2
 end
 
 def square_root(num)
